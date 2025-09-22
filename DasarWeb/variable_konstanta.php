0c0b2a06f947c810e2a3201fa402ba353fdf2dc8
dinaKSari <dinakumala7070@gmail.com>
2025-09-22 21:20:17 +0700

    menambahkan linereak

--- DasarWeb/variable_konstanta.php
+++ DasarWeb/variable_konstanta.php
@@ -2,11 +2,11 @@
 $angka1 = 10;
 $angka2 = 5;
 $hasil = $angka1 + $angka2;
-echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil";
+echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil \n";
 
 $benar = true;
 $salah = false;
-echo "variable benar: $benar, variabel salah: $salah";
+echo "variable benar: $benar, variabel salah: $salah \n";
 
 // mendefinisikan konstanta untuk nilai tetap
 define("NAMA_SITUS","WebsiteKu.com");
