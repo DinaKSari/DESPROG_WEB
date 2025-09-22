24f0190953b9e5662447ee0cebc4f48297ea8bc2
dinaKSari <dinakumala7070@gmail.com>
2025-09-23 01:40:57 +0700

    menambahkan array kosong

--- DasarWeb/array.php
+++ DasarWeb/array.php
@@ -11,5 +11,6 @@ echo "Daftar nilai siswa yang lulus: " . implode(', ', $nilaiLulus);
 
 //lanjut
 $daftarKaryawan = [['Alice', 7],['Bob', 3],['Charlie', 9],['David', 5],['Eva', 6],];
+$karyawanPengalamanLimaTahun = [];
 
 ?>
