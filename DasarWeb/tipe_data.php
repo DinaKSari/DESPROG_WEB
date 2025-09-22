b6e88b9aca3b7dcdba28b33ea1b2ea2c5b8a84fb
dinaKSari <dinakumala7070@gmail.com>
2025-09-22 21:51:10 +0700

    membuat echo untuk menampilkan hasil penjuamlahan dan nilai yang ada. lalu var dump untuk menampilkan tipe data variable ratarata dan isinya

--- DasarWeb/tipe_data.php
+++ DasarWeb/tipe_data.php
@@ -19,4 +19,11 @@ $nilaiIPA = 6.7;
 $nilaiBahasaIndonesia = 9.3;
 
 $rataRata = ($nilaiMatematika + $nilaiIPA + $nilaiBahasaIndonesia) / 3;
+
+echo "Matematika: {$nilaiMatematika} <br>";
+echo "IPA: {$nilaiIPA} <br>";
+echo "Bahasa Indonesia: {$nilaiBahasaIndonesia} <br>";
+echo "Rata-rata: {$rataRata} <br>";
+
+var_dump($rataRata);
 ?>
