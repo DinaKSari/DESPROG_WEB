2170646e9e47b26e4387d8e1dd61f372ade33962
dinaKSari <dinakumala7070@gmail.com>
2025-09-22 21:59:49 +0700

    menambahkan echo untuk menampilkan output

--- DasarWeb/tipe_data.php
+++ DasarWeb/tipe_data.php
@@ -42,4 +42,8 @@ $namaBelakang = 'Jakaria';
 $namaLengkap = "{$namaDepan} {$namaBelakang}";
 $namaLengkap2 = $namaDepan . ' ' . $namaBelakang;
 
+echo "Nama Depan: {$namaDepan} <br>";
+echo 'Nama Belakang: ' . $namaBelakang . '<br>';
+
+echo $namaLengkap;
 ?>
