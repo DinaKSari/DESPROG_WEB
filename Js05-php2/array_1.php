2e7087f9499eb307cdec5c33d2a7578f5cb53dfe
dinaKSari <dinakumala7070@gmail.com>
2025-09-28 16:14:24 +0700

    modifikasi agar menampilkan isi array menggunakan loop

--- Js05-php2/array_1.php
+++ Js05-php2/array_1.php
@@ -6,9 +6,11 @@
         <h2>Array Terindeks</h2>
         <?php
             $Listdosen=["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
-            echo $Listdosen [2] . "<br>";
-            echo $Listdosen [0] . "<br>";
-            echo $Listdosen [1] . "<br>";
+            $i = 0;
+            foreach($Listdosen as $list){
+                echo $Listdosen [$i] . "<br>";
+                $i++;
+            }
         ?>
     </body>
 </html>
