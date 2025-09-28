be9d8d9695d6a07e78631ef42e92eba80e3b4f3d
dinaKSari <dinakumala7070@gmail.com>
2025-09-28 19:25:55 +0700

    membuat fungsi perkenalan

---
+++ Js05-php2/function.php
@@ -0,0 +1,9 @@
+<?php
+function perkenalan(){
+echo "Assalamualaikum, ";
+echo "Perkenalkan, nama saya Dina<br/>";
+echo "Senang berkenalan dengan Anda<br/>";
+}
+//memanggil fungsi yang sudah dibuat
+perkenalan();
+?>
