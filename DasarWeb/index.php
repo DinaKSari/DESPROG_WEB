c3ceaa5c7db2c3917c02d8c8f65ea28250c82d22
dinaKSari <dinakumala7070@gmail.com>
2025-10-06 21:49:50 +0700

    membuat index.php untuk tugas query postgre

---
+++ DasarWeb/index.php
@@ -0,0 +1,33 @@
+<?php
+
+// --- KONFIGURASI KONEKSI POSTGRESQL ---
+$host = 'localhost';
+$port = '5432';
+$dbname = 'phpdatabase';
+$user = 'postgres';
+$pass = '12345';
+
+// Membuat koneksi
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass");
+if (!$conn) {
+    die('Koneksi gagal: ' . pg_last_error());
+}
+
+// Set encoding (opsional tapi dianjurkan)
+pg_set_client_encoding($conn, 'UTF8');
+
+// Ambil data dari tabel mahasiswa
+// Pakai alias agar array assoc tetap menggunakan key "Nama", "Nim", dst.
+$sql = 'SELECT
+            "Nim"       AS "Nim",
+            "Nama"      AS "Nama",
+            "Email"     AS "Email",
+            "Jurusan"   AS "Jurusan"
+        FROM "TB_Mahasiswa"
+        ORDER BY "Nim"';
+
+$result = pg_query($conn, $sql);
+if (!$result) {
+    die('Query gagal: ' . pg_last_error($conn));
+}
+?>
