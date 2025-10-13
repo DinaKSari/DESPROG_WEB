5f455e4f6d1dbe474ac40f2d7f1458e504505c8d
dinaKSari <dinakumala7070@gmail.com>
2025-10-13 20:19:26 +0700

    memodifikasi untuk langkah 18

--- DasarWeb/regex.php
+++ DasarWeb/regex.php
@@ -39,5 +39,11 @@ if (preg_match($pattern, $text, $matches)) {
 }
 
 //langkah 18
-
+$pattern = '/go{1,3}d/';
+$text = 'god is good.';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
+} else {
+    echo "Tidak ada yang cocok!";
+}
 ?>
