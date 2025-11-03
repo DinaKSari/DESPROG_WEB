4159cae064d4fac0bfde4dbd04deaf59ac08c133
dinaKSari <dinakumala7070@gmail.com>
2025-11-03 20:41:51 +0700

    mengubah query gabungan untuk mengambil data di table

--- CRUD/index.php
+++ CRUD/index.php
@@ -2,7 +2,7 @@
 require __DIR__ . '/koneksi.php';
 
 // Query gabungan
-$res = q("select (*) from peminjaman");
+$res = q('select id, nama_mahasiswa, nim, judul_buku, tanggal_pinjam, tanggal kembali, status from public.peminjaman order by id desc');
 
 $rows = pg_fetch_all($res) ?: [];
 ?>
